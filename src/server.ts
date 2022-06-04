8bbc043284614e86d89fc4587010f63e01ec8c0c
underscoreDev <godswill@zimmerman.team>
2022-06-04 19:43:20 +0100

    feat: connected database to mongodb

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,18 @@
-import app from "./app";
 import "dotenv/config";
+import app from "./app";
+import mongoose from "mongoose";
+const { PORT, HOST, DATABASE } = process.env;
 
-const { PORT, HOST } = process.env;
+const dbConnect = async () => {
+  try {
+    await mongoose.connect(DATABASE !== undefined ? DATABASE : "");
+    console.log("************DATABASE CONNECTED************");
+  } catch (error) {
+    throw error;
+  }
+};
 
-app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
+app.listen(PORT, async () => {
+  await dbConnect();
   console.log(`Server started on http://${HOST}:${PORT}`);
 });
