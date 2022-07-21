f33502996562e098924f19ffdd317d18993aef91
underscoreDev <godswill@zimmerman.team>
2022-07-22 00:08:32 +0100

    feat: finished error handling

--- src/controllers/handleAppError.controller.ts
+++ src/controllers/handleAppError.controller.ts
@@ -1,4 +1,26 @@
 import { ErrorRequestHandler, Response } from "express";
+import { AppError } from "../middlewares/handleAppError.middleware";
+
+const handleCastErrorDB = (err: { path: any; value: any }) => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = (err: { errmsg: { match: (arg0: RegExp) => any[] } }) => {
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+  console.log(value);
+
+  const message = `Duplicate field value: ${value}. Please use another value!`;
+  return new AppError(message, 400);
+};
+const handleValidationErrorDB = (err: {
+  errors: { [s: string]: unknown } | ArrayLike<unknown>;
+}) => {
+  const errors = Object.values(err.errors).map((el: any) => el.message);
+
+  const message = `Invalid input data. ${errors.join(". ")}`;
+  return new AppError(message, 400);
+};
 
 const sendDevError = (err: { statusCode: any; status: any; message: any }, res: Response) => {
   res.status(err.statusCode).json({
@@ -27,5 +49,15 @@ export const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => 
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
 
-  process.env.NODE_ENV === "development" ? sendDevError(err, res) : sendProdError(err, res);
+  if (process.env.NODE_ENV === "development") {
+    sendDevError(err, res);
+  } else if (process.env.NODE_ENV === "production") {
+    let error = { ...err };
+
+    if (error.name === "CastError") error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (error.name === "ValidationError") error = handleValidationErrorDB(error);
+
+    sendProdError(error, res);
+  }
 };
--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,12 @@ import app from "./app";
 import mongoose from "mongoose";
 const { PORT, HOST, DATABASE_LOCAL } = process.env;
 
+process.on("uncaughtException", (err) => {
+  console.log("****** UNCAUGHT EXCEPTION 🔥🔥🔥 SHUTTING DOWN *****");
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 const dbConnect = async () => {
   try {
     await mongoose.connect(DATABASE_LOCAL as string);
@@ -12,7 +18,13 @@ const dbConnect = async () => {
   }
 };
 
-app.listen(PORT, async () => {
+const server = app.listen(PORT, async () => {
   await dbConnect();
   console.log(`Server started on http://${HOST}:${PORT}`);
+});
+
+process.on("unhandledRejection", (err: any) => {
+  console.log(err.name, err.message);
+  console.log("****** UMHANDLED REJECTION 🔥🔥🔥 SHUTTING DOWN *****");
+  server.close(() => process.exit(1));
 });
