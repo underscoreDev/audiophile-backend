56bf4234ba1a1cf2e55038b2ab28021cc46d159d
underscoreDev <godswill@zimmerman.team>
2022-08-22 14:17:14 +0100

    feat: update config.yml, package.json and refactored tests

--- src/tests/app.test.ts
+++ src/tests/app.test.ts
@@ -4,7 +4,7 @@ import app from "../app";
 const request = supertest(app);
 describe("Test root server", () => {
   it("returns status code 200", async () => {
-    const { statusCode } = await request.get("/");
+    const { statusCode } = await request.get("/api/v1");
     expect(statusCode).toBe(200);
   });
 });
